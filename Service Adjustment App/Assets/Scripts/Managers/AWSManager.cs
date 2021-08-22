8b90cd80b98c78a74f8126f6ab13cb067f58e7a3
SteveTheDragon <draconis36@gmail.com>
2021-08-21 18:12:33 -0700

    Finished insurance app and analytics

--- Service Adjustment App/Assets/Scripts/Managers/AWSManager.cs
+++ Service Adjustment App/Assets/Scripts/Managers/AWSManager.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
-using UnityEngine.UI;
-using Amazon.S3;
-using Amazon.S3.Model;
-using Amazon.Runtime;
-using System.IO;
-using System;
-using Amazon.S3.Util;
 using System.Collections.Generic;
-using Amazon.CognitoIdentity;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Amazon;
+using Amazon.CognitoIdentity;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Util;
+using Amazon.S3.Model;
+
 
 public class AWSManager : MonoBehaviour
 {
@@ -18,10 +22,8 @@ public class AWSManager : MonoBehaviour
     {
         get
         {
-            if(_instance == null)
-            {  
-               Debug.LogError("AWSManager is NULL"); 
-            }
+            if(_instance == null) Debug.LogError("AWSManager is NULL"); 
+
             return _instance;
         }
     }
@@ -51,6 +53,8 @@ public class AWSManager : MonoBehaviour
     }
 
     
+    private string _bucketName = "std-service-app-case-files";
+    
     private void Awake()
     {
         _instance = this;
@@ -59,6 +63,7 @@ public class AWSManager : MonoBehaviour
         UnityInitializer.AttachToGameObject(this.gameObject);
         AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
         
+        /*
         S3Client.ListBucketsAsync(new ListBucketsRequest(), (responseObject) =>
         {
             
@@ -75,7 +80,7 @@ public class AWSManager : MonoBehaviour
             }
         });
 
-
+        */
     }
 
     public void UploadToS3(string path, string caseID)
@@ -84,7 +89,7 @@ public class AWSManager : MonoBehaviour
 
         PostObjectRequest request = new PostObjectRequest()
         {
-            Bucket = "std-service-app-case-files",
+            Bucket = _bucketName,
             Key = "case#"+caseID,
             InputStream = stream,
             CannedACL = S3CannedACL.Private,
@@ -96,6 +101,7 @@ public class AWSManager : MonoBehaviour
             if(responseObj.Exception == null)
             {
                 Debug.Log("Successfully posted to bucket");
+                SceneManager.LoadScene(0);
             }
             else
             {
@@ -103,6 +109,99 @@ public class AWSManager : MonoBehaviour
             }
         });
 
+    }
+
+    public void GetList(string caseNumber, Action onComplete = null)
+    {
+
+        string target = "case#" + caseNumber;
+
+        Debug.Log("AWSManager :: GetList()");
+        var request = new ListObjectsRequest()
+        {
+            BucketName = _bucketName
+        };
+
+        S3Client.ListObjectsAsync(request, (responseObject) => 
+        {
+            if(responseObject.Exception == null)
+            {
+                bool caseFound = responseObject.Response.S3Objects.Any(obj => obj.Key == target);
+
+                if (caseFound == true)
+                {
+                    Debug.Log("Found the case!");
+                    S3Client.GetObjectAsync(_bucketName, target, (responseObject) => 
+                    {                         
+                        //read the data and apply it to a casefile (object) to be used.
+                        //i.e. if we successfully download the file, it will be placed in responseObject.
+                        
+                        //check if response stream is empty or null.
+                        if (responseObject.Response.ResponseStream != null)
+                        {
+                            //byte array to store data from file
+                            byte[] data  = null;
+                            //this will be populated in the memory stream.
+
+                            //create a stream reader to read the response data.
+                            //we use a using so stream is disposed when done using it.
+                            using(StreamReader reader = new StreamReader(responseObject.Response.ResponseStream))
+                            {
+                                //access memory stream
+                                //we open a new stream but a memory stream
+                                //this just gives us -access- to the memory of what we downloaded.
+                                //to actually recreate our casefile object, we'll need to use
+                                //memory.write() to populate a databyte array with the memorystream data.
+                                //memory.write() also needs buffer, offset, and count.
+
+                                using(MemoryStream memory = new MemoryStream())
+                                {
+                                    var buffer = new byte[512];
+                                    var bytesRead = default(int); // parsing byte.
+                                    //create a while loop to parse through bytes read.
+                                    while((bytesRead = reader.BaseStream.Read(buffer, 0, buffer.Length))>0)
+                                    {
+                                        //while there's bytes to be read and the file length > 0, we'll write all the bytes we're reading.
+                                        memory.Write(buffer,0,bytesRead);
+                                    } 
+                                    //place the memory into the byte array called data we created earlier.
+                                    data = memory.ToArray();
+                                }
+                            }
+
+                            //Convert those bytes to a case (Object)
+                            //we'll use yet another memory stream
+
+                            using (MemoryStream memory = new MemoryStream(data))
+                            {
+                                //we're going to need a binary formatter to reconstruct this stream.
+                                //be sure to include the System.Runtime.Serialization.Formatters.Binary namespace
+
+                                BinaryFormatter _binaryFormatter = new BinaryFormatter();
+                                Case downloadedCase = (Case)_binaryFormatter.Deserialize(memory);
+                                Debug.Log("Downloaded Case Name: " + downloadedCase.name);
+                                UIManager.Instance.activeCase = downloadedCase;
+                                
+                                if(onComplete != null)
+                                {
+                                    onComplete();
+                                }
+                                    
+
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    Debug.Log("Case not found.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Error getting list of items off s3" + responseObject.Exception);
+            }
+        });
     }
 
     
--- Service Adjustment App/Assets/Scripts/Panels/SearchPanel.cs
+++ Service Adjustment App/Assets/Scripts/Panels/SearchPanel.cs
@@ -7,8 +7,21 @@ public class SearchPanel : MonoBehaviour, IPanel
 {
 
     public InputField caseNumberInput;
+
+    public SelectPanel selectPanel;
+
     public void ProcessInfo()
     {
+        //download list of all objects inside of the service-app-casefiles bucket.
+
+        AWSManager.Instance.GetList(caseNumberInput.text, ()=>{
+            selectPanel.gameObject.SetActive(true);
+        });
+
+        
+        
+        //compare those to the caseNumberInput
+        //if we find a match, download the object
 
     }
 }
